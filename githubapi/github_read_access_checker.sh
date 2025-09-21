ac5ced393936835492209d218ba63711f6b70ee1
Its me Vishnu M V <vishnumv550@gmail.com>
2025-09-22 01:14:58 +0530

    Update github_read_access_checker.sh

--- githubapi/github_read_access_checker.sh
+++ githubapi/github_read_access_checker.sh
@@ -4,7 +4,7 @@
 #######################################################################################
 #Description    : Script to list all the users having access to a given Github repo
 #Author         : Vishnu M V
-#Version        : 1.0
+#Version        : 1.1
 #Created On     : 21/09/25
 #Usage          : ./github_read_access_checker.sh <repo_owner> <repo_name>
 #Requirements   :
@@ -12,9 +12,8 @@
 #   - GitHub Personal Access Token (PAT) with 'repo' scope
 #   - Environment variables: username, token
 #License        : MIT
-#######################################################################################
+#########################################################################################
 
-helper()
 
 # GitHub API URL
 API_URL="https://api.github.com"
@@ -26,6 +25,31 @@ TOKEN=$token
 # User and Repository information
 REPO_OWNER=$1
 REPO_NAME=$2
+
+# Helper function to validate input and pre-requisites
+function helper {
+        expected_cmd_args=2
+        if [ $# -ne $expected_cmd_args ];
+         then
+           echo "Incorrect Usage"
+           echo "Usage: ./github_read_access_checker.sh <org_name> <repo_name> "
+                exit 1
+        fi
+
+        if [[ -z "$USERNAME" || -z "$TOKEN" ]]; then
+        echo "Please export 'username' and 'token' before running the script."
+        exit 1
+        fi
+
+        if ! command -v jq &>/dev/null; then
+        echo "'jq' is required but not installed. Run: sudo apt install jq"
+        exit 1
+        fi
+                }
+
+# Call helper at the start
+helper "$@"
+
 
 # Function to make a GET request to the GitHub API
 function github_api_get {
@@ -51,13 +75,7 @@ function list_users_with_read_access {
         echo "$collaborators"
     fi
 }
-function helper{
-expected_cmd_args=2
-if [ $# -ne $expected_cmd_args]; then
- echo "Please execute the script with required cmd arguments"
- echo "asd"
 
-}
 
 # Main script
 
